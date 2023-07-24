4702e52594b8404fe21353622445bf882d2b673b
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-07-24 21:59:05 +0200

    Creación de rutas y maquetación de la página principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="main-page-container border border-black">
+          <h1 className="main-page-title">Carlos Bustos</h1>
+          <p className="main-page-description">Front End Developer</p>
+          <ul className="main-page-navbar">
+            <li><Link href="/home">Home</Link></li>
+            <li><Link href="/projects">Proyectos</Link></li>
+            <li><Link href="/contact">Contacto</Link></li>
+          </ul>
+        </main>
+
+        <div className="main-page-container-content">
+          {children}
+        </div>
+
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-
 export default function Home() {
   return (
-    <main className="main-page-container border border-black">
-      <h1 className="main-page-title">Carlos Bustos</h1>
-      <p className="main-page-description">Front End Developer</p>
-    </main>
+    <>
+    </>
   )
 }
