5beaae0fa0ae349fe67bd0266b0faae34e8d4c7c
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-07-27 21:37:24 +0200

    Prueba de traduccíon de página con middleware

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,9 @@
 import '../globals.css'
 
 import type { Metadata } from 'next'
-import { useLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import { headers } from "next/headers";
+import { useLocale } from 'next-intl';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
@@ -22,10 +22,11 @@ export default function LocaleLayout({
   params: any
 }) {
   const locale = useLocale()
-  //const currentRoute = usePathname()
   const activeStyle = 'navbar-active'
   const nonActiveStyle = 'navbar-nonactive'
   const t = useTranslations();
+  const headersList = headers();
+  const currentRoute = headersList.get("pathname")
 
   if (params.locale !== locale) {
     notFound();
@@ -38,9 +39,9 @@ export default function LocaleLayout({
           <h1 className="main-page-title">Carlos Bustos</h1>
           <p className="main-page-description">Front End Developer</p>
           <ul className="main-page-navbar">
-            <li><Link href={locale + "/home"} /*className={currentRoute === '/home' ? activeStyle : nonActiveStyle}*/>Home</Link></li>
-            <li><Link href={locale + "/projects"} /*className={currentRoute === '/projects' ? activeStyle : nonActiveStyle}*/>{t("MENU_PROJECTS")}</Link></li>
-            <li><Link href={locale + "/contact"} /*className={currentRoute === '/contact' ? activeStyle : nonActiveStyle}*/>Contacto</Link></li>
+            <li><Link href={"/" + locale + "/home"} className={currentRoute?.includes("/home") ? activeStyle : nonActiveStyle}>Home</Link></li>
+            <li><Link href={"/" + locale + "/projects"} className={currentRoute?.includes("/projects") ? activeStyle : nonActiveStyle}>{t("MENU_PROJECTS")}</Link></li>
+            <li><Link href={"/" + locale + "/contact"} className={currentRoute?.includes("/contact") ? activeStyle : nonActiveStyle}>Contacto</Link></li>
           </ul>
         </main>
         <div className="main-page-container-content">
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextFetchEvent, NextMiddleware } from "next/server";
 
-export default createMiddleware({
-    defaultLocale: 'es',
-    locales: ['es', 'en']
-});
+export default withExtraMiddleware(
+    createMiddleware({
+        defaultLocale: 'es',
+        locales: ['es', 'en']
+    })
+);
+
+function withExtraMiddleware(next: NextMiddleware) {
+    return async (request: any, event: NextFetchEvent) => {
+
+        request.headers.set("pathname", request.nextUrl.pathname);
+        const response = await next(request, event);
+
+        return response
+    };
+}
 
 export const config = {
     matcher: ['/((?!api|_next|.*\\..*).*)']
