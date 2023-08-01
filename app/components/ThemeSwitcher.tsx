d4d28273d3db79ffb18a2c383578421571d28408
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-08-01 23:40:34 +0200

    Cambio de diseño en el menú de temas

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,13 +1,14 @@
 "use client";
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-import { useTranslations } from "next-intl";
-
+import { useState, useEffect } from "react"
+import { useTheme } from "next-themes"
+import { useTranslations } from "next-intl"
 
 export const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
     const t = useTranslations('ThemeSwitcher')
+    const activeStyle = 'cursor-pointer mr-[5px] navbar-active'
+    const nonActiveStyle = 'cursor-pointer mr-[5px] navbar-nonactive'
 
     useEffect(() => {
         setMounted(true);
@@ -21,11 +22,8 @@ export const ThemeSwitcher = () => {
     return (
         <>
             <p className="text-base mt-[20px]">{t("CHANGE_THEME")}</p>
-            <button className={`w-fit p-2 mb-[10px] duration-200 bg-black text-white text-base dark:bg-white dark:text-black`}
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                {theme === "light" ? t("DARK") : t("LIGHT")}
-            </button>
+            <a className={theme === "dark" ? activeStyle : nonActiveStyle} onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{t("DARK")}</a>
+            <a className={theme === "light" ? activeStyle : nonActiveStyle} onClick={() => setTheme(theme === "light" ? "dark" : "light")}>{t("LIGHT")}</a>
         </>
-
     );
 };
