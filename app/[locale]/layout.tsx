80ff9599af4881c78c8bf7a82f158068a5f7e149
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-08-08 22:20:04 +0200

    Componente SocialButtons e integración en la página principal

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from './theme-provider'
 import { ThemeSwitcher } from '../components/ThemeSwitcher'
 import MenuLang from '../components/MenuLang'
 import MenuResponsive from '../components/MenuResponsive'
+import SocialButtons from '../components/SocialButtons'
 
 export const metadata: Metadata = {
   title: 'Carlos Bustos Portfolio'
@@ -33,12 +34,13 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body className='bg-slate-50 dark:bg-[#000000]'>
+      <body className='bg-white dark:bg-[#000000]'>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             <main className="main-page-container border border-black dark:border-white m-[40px] p-[40px]">
               <h1 className="main-page-title">Carlos Bustos</h1>
               <p className="main-page-description">Front End Developer</p>
+              <SocialButtons />
               <div className='md:block max-md:hidden'>
                 <ThemeSwitcher />
                 <MenuLang />
