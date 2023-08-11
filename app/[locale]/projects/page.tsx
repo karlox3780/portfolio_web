a2624b4cd87539591f45eb1923a034b163a7eca4
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-08-11 21:47:00 +0200

    Maquetación de página de Proyectos, creación de componentes y traducciones de texto

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,7 +1,43 @@
-export default function projects() {
+import Css3Icon from "@/app/components/TecnologyIcons/Css3Icon"
+import HandleBarsIcon from "@/app/components/TecnologyIcons/HandleBarsIcon"
+import NextJsIcon from "@/app/components/TecnologyIcons/NextJsIcon"
+import NodeJsIcon from "@/app/components/TecnologyIcons/NodeJsIcon"
+import ReactJsIcon from "@/app/components/TecnologyIcons/ReactJsIcon"
+import { useTranslations } from "next-intl"
+
+export default function Projects() {
+    const t = useTranslations('ProjectsPage')
+
     return (
-        <>
-            <h1>Projects</h1>
-        </>
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="font-bold text-[26px] mr-[5px]">{t("Title")}</h1>
+            <div className="w-[50%] mt-[20px] max-lg:w-[90%] max-md:w-[100%]">
+                <div className="flex items-center justify-center">
+                    <h1 className="font-bold text-[20px] mr-[5px]">{t("GiftyNextJs.Title")}</h1>
+                    <NodeJsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <NextJsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <Css3Icon className="w-[30px] fill-black dark:fill-white" />
+                </div>
+                <p>{t("GiftyNextJs.Paragraph")}</p>
+            </div>
+            <div className="w-[50%] mt-[20px] max-lg:w-[90%] max-md:w-[100%]">
+                <div className="flex items-center justify-center">
+                    <h1 className="font-bold text-[20px] mr-[5px]">{t("Gifty.Title")}</h1>
+                    <NodeJsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <ReactJsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <Css3Icon className="w-[30px] fill-black dark:fill-white" />
+                </div>
+                <p>{t("Gifty.Paragraph")}</p>
+            </div>
+            <div className="w-[50%] mt-[20px] max-lg:w-[90%] max-md:w-[100%]">
+                <div className="flex items-center justify-center">
+                    <h1 className="font-bold text-[20px] mr-[5px]">{t("ChicagoArt.Title")}</h1>
+                    <NodeJsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <HandleBarsIcon className="w-[30px] fill-black dark:fill-white" />
+                    <Css3Icon className="w-[30px] fill-black dark:fill-white" />
+                </div>
+                <p>{t("ChicagoArt.Paragraph")}</p>
+            </div>
+        </div >
     )
 }
